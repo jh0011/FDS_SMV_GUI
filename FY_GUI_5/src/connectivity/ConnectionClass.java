95d094daf1177ce8cd95ba3d1d46d6545e2d7347
jh0011 <jashini.h@gmail.com>
2020-11-30 15:21:09 +0800

    Minor changes

--- FY_GUI_5/src/connectivity/ConnectionClass.java
+++ FY_GUI_5/src/connectivity/ConnectionClass.java
@@ -16,7 +16,6 @@ public class ConnectionClass {
 			Class.forName("com.mysql.cj.jdbc.Driver"); 
 			
 			connection = DriverManager.getConnection(url, userName, password); //WORKS
-			//connection = DriverManager.getConnection("mysql://zsl9m6thl6ff1n5u:tlzvdrrd8d2b0j9d@aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/n92wmp3nrx2aa58q");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
